9acb4cc0a09f2fbe643d338889c08c4bdbd0a561
lgacerbi <lgacerbi@gmail.com>
2023-09-03 14:00:10 -0300

    chore: use auth handler routes

--- Backend/Auth API/src/router.ts
+++ Backend/Auth API/src/router.ts
@@ -1,12 +1,15 @@
 import "express-async-errors";
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
 import helmet from "helmet";
 import errorMiddleware from "./middlewares/error-middleware";
+import AuthHandler from "./handlers/auth-handler";
 
 const Router = express();
 
 Router.use(helmet());
 Router.use(express.json());
+
+Router.use(AuthHandler);
 
 Router.use(errorMiddleware);
 
