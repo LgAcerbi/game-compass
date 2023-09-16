0e38a0baf6d20f4cc3f9430fc0ce68c863929d86
lgacerbi <lgacerbi@gmail.com>
2023-09-16 13:14:18 -0300

    build: lists and auth api

--- Backend/startup.bat
+++ Backend/startup.bat
@@ -2,4 +2,8 @@ docker network create -d bridge game-compass
 
 docker build --pull --rm -f "Users API\dockerfile" -t game-compass_users-api:latest "Users API"
 
+docker build --pull --rm -f "Auth API\dockerfile" -t game-compass_auth-api:latest "Auth API"
+
+docker build --pull --rm -f "Lists API\dockerfile" -t game-compass_lists-api:latest "Lists API"
+
 docker-compose -p game-compass up -d
