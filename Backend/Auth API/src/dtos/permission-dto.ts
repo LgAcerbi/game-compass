0bc4ed9b9f2f1542957ffd537e4c67ccad352ae2
lgacerbi <lgacerbi@gmail.com>
2023-09-17 15:48:47 -0300

    fix: use type string when refering to another interface ids

--- Backend/Auth API/src/dtos/permission-dto.ts
+++ Backend/Auth API/src/dtos/permission-dto.ts
@@ -1,7 +1,5 @@
-import ResourceDTO from "./resource-dto";
-
 export default interface PermissionDTO {
     id: string;
     name: string;
-    resources: Array<Pick<ResourceDTO, "id">>;
+    resources: Array<string>;
 }
--- Backend/Auth API/src/dtos/role-dto.ts
+++ Backend/Auth API/src/dtos/role-dto.ts
@@ -1,7 +1,5 @@
-import PermissionDTO from "./permission-dto";
-
 export default interface RoleDTO {
     id: string;
     name: string;
-    permissions: Array<Pick<PermissionDTO, "id">>;
+    permissions: Array<string>;
 }
