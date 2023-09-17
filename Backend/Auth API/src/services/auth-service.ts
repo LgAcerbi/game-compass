786d96161933b02ecdc551d458b8accf4a7a7a0a
lgacerbi <lgacerbi@gmail.com>
2023-09-17 15:49:03 -0300

    feat: evaluate endpoint

--- Backend/Auth API/src/services/auth-service.ts
+++ Backend/Auth API/src/services/auth-service.ts
@@ -1,12 +1,19 @@
+import { sign, verify } from "jsonwebtoken";
+import RoleService from "./role-service";
+import PermissionService from "./permission-service";
+import ResourceService from "./resource-service";
+import UserService from "./user-service";
+import AuthUserModel from "../models/auth-user-model";
+import EvaluateDTO from "../dtos/evaluate-dto";
 import LoginDTO from "../dtos/login-dto";
 import RegisterDTO from "../dtos/register-dto";
-import HttpError from "../helpers/http-error";
+import JwtPayloadDTO from "../dtos/jwt-payload-dto";
 import PasswordHelper from "../helpers/password";
-import AuthUserModel from "../models/auth-user-model";
-import UserService from "./user-service";
-import { sign } from "jsonwebtoken";
+import HttpError from "../helpers/http-error";
 
-const { TOKEN_SECRET, TOKEN_EXPIRES_IN = 86400 } = process.env;
+const { JWT_TOKEN_SECRET, JWT_TOKEN_EXPIRES_IN = 86400 } = process.env;
+
+// TO-DO !important Implement sessions for each token with REDIS
 
 // TO-DO Get minimum age restrictions in database
 const minimumAgeRestrictionByCountry = new Map([
@@ -62,12 +69,12 @@ class AuthService {
             roles: ["user"],
         });
 
-        const token = sign({ id }, String(TOKEN_SECRET), { expiresIn: Number(TOKEN_EXPIRES_IN) });
+        const token = sign({ id }, String(JWT_TOKEN_SECRET), { expiresIn: Number(JWT_TOKEN_EXPIRES_IN) });
 
         return { token };
     }
 
-    static async login(loginData: LoginDTO) {
+    static async login(loginData: LoginDTO): Promise<{ token: string }> {
         const foundAuthUser = await AuthUserModel.findAuthUserByEmail(loginData.email);
 
         if (!foundAuthUser) {
@@ -80,12 +87,46 @@ class AuthService {
             throw new HttpError(401, "Invalid email or password");
         }
 
-        const token = sign({ id: foundAuthUser.userId }, String(TOKEN_SECRET), { expiresIn: TOKEN_EXPIRES_IN });
+        const token = sign({ id: foundAuthUser.userId, email: foundAuthUser.email }, String(JWT_TOKEN_SECRET), {
+            expiresIn: Number(JWT_TOKEN_EXPIRES_IN),
+        });
 
         return { token };
     }
 
-    static async evaluate() {}
+    static async evaluate(evaluateData: EvaluateDTO): Promise<{ evaluated: boolean }> {
+        const decodedToken = verify(evaluateData.token, String(JWT_TOKEN_SECRET), { complete: true });
+
+        const { email } = decodedToken.payload as JwtPayloadDTO;
+
+        const foundUserPromise = AuthUserModel.findAuthUserByEmail(email);
+        const foundResourcePromise = ResourceService.findResourceByEndpointData({
+            method: evaluateData.method,
+            endpoint: evaluateData.endpoint,
+            application: evaluateData.application,
+        });
+
+        const [foundUser, foundResource] = await Promise.all([foundUserPromise, foundResourcePromise]);
+
+        if (!foundUser) {
+            // TO-DO Expire the session
+
+            throw new HttpError(401, "Session expired");
+        }
+
+        const foundRoles = await RoleService.findRolesByIds(foundUser.roles);
+        const foundPermissions = await PermissionService.findPermissionsByIds(foundRoles.map((role) => role.id));
+
+        const resourcesWithPermission = foundPermissions.flatMap((permission) => permission.resources);
+
+        const hasPermissionOnResource = resourcesWithPermission.find((resourceId) => resourceId === foundResource.id);
+
+        if (!hasPermissionOnResource) {
+            throw new HttpError(403, "Not enough privileges to access this resource");
+        }
+
+        return { evaluated: true };
+    }
 }
 
 export default AuthService;
